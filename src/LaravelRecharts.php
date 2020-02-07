caa6b2ecd1f8ecbdba6f241d2a88c4f90eb148d7
Kaishiyoku <dmp@nfze.de>
2020-02-07 22:32:26 +0100

    renamed method

--- src/LaravelRecharts.php
+++ src/LaravelRecharts.php
@@ -7,10 +7,10 @@ use Ramsey\Uuid\Uuid;
 
 class LaravelRecharts
 {
-    public function makeLineChart(array $elements, array $data, int $height)
+    public function makeChart(array $elements, array $data, int $height)
     {
         $chartId = 'laravel-rechart-' . Uuid::uuid4();
 
-        return View::make('recharts::line_chart', compact('chartId', 'elements', 'data', 'height'));
+        return View::make('recharts::chart', compact('chartId', 'elements', 'data', 'height'));
     }
 }
